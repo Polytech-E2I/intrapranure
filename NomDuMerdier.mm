f105ede65efe050e65f5b9317ea4d2226cd23fde
Robin SHAMSNEJAD <rsham@free.fr>
2023-04-19 11:46:58 +0200

    Add stuff

---
+++ NomDuMerdier.mm
@@ -0,0 +1,111 @@
+<map version="freeplane 1.9.13">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Nom pour notre merdier" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1680676404559"><hook NAME="MapStyle" background="#2e3440" zoom="0.592">
+    <properties fit_to_viewport="false" show_icon_for_attributes="true" show_note_icons="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" associatedTemplateLocation="template:/dark_nord_template.mm"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_671184412" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#484747" BACKGROUND_COLOR="#eceff4" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#88c0d0" WIDTH="2" TRANSPARENCY="255" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_671184412" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#81a1c1" WIDTH="3" DASH="SOLID"/>
+<richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" BORDER_WIDTH="1.9 px">
+<edge STYLE="bezier" COLOR="#81a1c1" WIDTH="3"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ebcb8b">
+<icon BUILTIN="clock2"/>
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating" COLOR="#484747">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#e5e9f0" BACKGROUND_COLOR="#5e81ac" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#5e81ac"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_779275544" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#bf616a">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_779275544"/>
+<font SIZE="14"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#484747" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font NAME="Ubuntu" SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#eceff4" BACKGROUND_COLOR="#d08770" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt">
+<font NAME="Ubuntu" SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#3b4252" BACKGROUND_COLOR="#ebcb8b">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#2e3440" BACKGROUND_COLOR="#a3be8c">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#2e3440" BACKGROUND_COLOR="#b48ead">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" BACKGROUND_COLOR="#81a1c1">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BACKGROUND_COLOR="#88c0d0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BACKGROUND_COLOR="#8fbcbb">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#d8dee9">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BACKGROUND_COLOR="#e5e9f0">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BACKGROUND_COLOR="#eceff4">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<node TEXT="Champ lexical du gîte" POSITION="right" ID="ID_1226945437" CREATED="1680678025295" MODIFIED="1680678047211">
+<node TEXT="Francé" ID="ID_285611346" CREATED="1680677636003" MODIFIED="1680677641210">
+<node TEXT="gîte" ID="ID_943612227" CREATED="1680676405764" MODIFIED="1680676408563"/>
+<node TEXT="domaine" ID="ID_1803771523" CREATED="1680676408892" MODIFIED="1680676421389"/>
+<node TEXT="résidence" ID="ID_1825111003" CREATED="1680676416521" MODIFIED="1680676427239"/>
+<node TEXT="lieu" ID="ID_734152886" CREATED="1680676427480" MODIFIED="1680676431977"/>
+<node TEXT="maison" ID="ID_59262460" CREATED="1680676432321" MODIFIED="1680676434786"/>
+<node TEXT="hôtel" ID="ID_810716384" CREATED="1680676435220" MODIFIED="1680676440123"/>
+<node TEXT="auberge" ID="ID_1711542410" CREATED="1680676440345" MODIFIED="1680676467112"/>
+<node TEXT="château" ID="ID_150956016" CREATED="1680676467328" MODIFIED="1680676498676"/>
+<node TEXT="logis" ID="ID_1246365248" CREATED="1680676592393" MODIFIED="1680676593875"/>
+<node TEXT="palace" ID="ID_1399498416" CREATED="1680676598113" MODIFIED="1680676599545"/>
+<node TEXT="palais" ID="ID_1759627213" CREATED="1680676603758" MODIFIED="1680676604733"/>
+</node>
+<node TEXT="Angliche" ID="ID_144897545" CREATED="1680677648939" MODIFIED="1680677654235">
+<node TEXT="lodging" ID="ID_1819600008" CREATED="1680677655582" MODIFIED="1680677658385"/>
+<node TEXT="resort" ID="ID_1080276330" CREATED="1680677658788" MODIFIED="1680677660807"/>
+<node TEXT="guesthouse" ID="ID_672624315" CREATED="1680677661218" MODIFIED="1680677664147"/>
+<node TEXT="accommodations" ID="ID_1918705321" CREATED="1680677664549" MODIFIED="1680677686779"/>
+<node TEXT="inn" ID="ID_1311306794" CREATED="1680677680159" MODIFIED="1680677685445"/>
+</node>
+</node>
+<node TEXT="Champ lexical de la choucroute" POSITION="left" ID="ID_374413698" CREATED="1680678053936" MODIFIED="1680678514138">
+<node TEXT="Francé" ID="ID_724434794" CREATED="1680678060961" MODIFIED="1680678063587">
+<node TEXT="créatif/créativité" ID="ID_13626804" CREATED="1680676410749" MODIFIED="1680676415879"/>
+<node TEXT="imagination" ID="ID_1549129714" CREATED="1680678084845" MODIFIED="1680678091951"/>
+<node TEXT="tournée" ID="ID_1643868020" CREATED="1680678092370" MODIFIED="1680678498242"/>
+<node TEXT="" ID="ID_1423280145" CREATED="1680678498763" MODIFIED="1680678498763"/>
+</node>
+</node>
+</node>
+</map>
